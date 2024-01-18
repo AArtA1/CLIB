0853de456bea504a543439db9d1a7497c910202c
Artak Aslanyan <aaaslanyan_2@edu.hse.ru>
2024-01-18 16:27:08 +0300

    Added tests for windowing

--- test/clib/Image.cpp
+++ test/clib/Image.cpp
@@ -46,42 +46,111 @@ TEST_CASE("Test Window")
 
     std::vector<std::vector<ff>> arr(4, std::vector<ff>(4));
 
+    int counter = 0;
     for (int i = 0; i < arr.size(); ++i)
     {
         for (int j = 0; j < arr[0].size(); ++j)
         {
-            arr[i][j] = ff_(i+j+1);
+            arr[i][j] = ff_(++counter);
         }
     }
+
+    for(auto it : arr){
+        for(auto val : it){
+            std::cout << val.to_float() << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
 
     img vv(arr);
 
     // test case 1
     auto test_1 = img::get_window(vv, {1, 1}, {3, 3});
-    // auto right_1 =
-    //     std::vector<std::vector<ff>>{{ff_(6), ff_(5), ff_(6)}, {ff_(2), ff_(1), ff_(2)}, {ff_(6), ff_(5), ff_(6)}};
-    // if (test_1 != right_1)
-    // {
-    //     CHECK(false);
-    // }
+    for(auto it : test_1.vv()){
+        for(auto val : it){
+            std::cout << val.to_float() << " ";
+        }
+        std::cout << std::endl;
+    }
 
-    // // test case 2
-    // auto test_2 = img::get_window(vv, {3, 0}, {3, 3});
-    // auto right_2 = std::vector<std::vector<ff>>{
-    //     {ff_(10), ff_(9), ff_(10)}, {ff_(14), ff_(13), ff_(14)}, {ff_(10), ff_(9), ff_(10)}};
-    // if (test_2 != right_2)
-    // {
-    //     CHECK(false);
-    // }
+    auto right_1 =
+         std::vector<std::vector<ff>>{{ff_(1), ff_(2), ff_(3)}, {ff_(5), ff_(6), ff_(7)}, {ff_(9), ff_(10), ff_(11)}};
+    CHECK(test_1.vv() == right_1);
 
-    // // test case 3
-    // auto test_3 = get_window(vv,{1,1},{3,3});
-    // auto right_3 = std::vector<std::vector<ff>>{{1,2,3},{5,6,7},{9,10,11}};
-    // if(test_3 != right_3){
-    //     return false;
-    //}
+
+    std::cout << std::endl;
+
+    // test case 2
+    auto test_2 = img::get_window(vv, {0, 0}, {3, 3});
+
+    for(auto it : test_2.vv()){
+        for(auto val : it){
+            std::cout << val.to_float() << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    auto right_2 = std::vector<std::vector<ff>>{
+        {ff_(6), ff_(5), ff_(6)}, {ff_(2), ff_(1), ff_(2)}, {ff_(6), ff_(5), ff_(6)}};
+    CHECK(test_2.vv() == right_2);
+
     
-    CHECK(true);
+    // test case 3
+    auto test_3 = img::get_window(vv,{3,0},{3,3});
+    
+    auto right_3 = std::vector<std::vector<ff>>{
+        {ff_(10), ff_(9), ff_(10)}, {ff_(14), ff_(13), ff_(14)}, {ff_(10), ff_(9), ff_(10)}};
+    CHECK(test_3.vv() == right_3);
 }
+
+
+TEST_CASE("Test Convolution")
+{
+    BOOST_LOG_SCOPED_THREAD_TAG("Tag", "Convolution");
+
+    std::vector<std::vector<ff>> arr(4, std::vector<ff>(4));
+
+    int counter = 0;
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        for (int j = 0; j < arr[0].size(); ++j)
+        {
+            arr[i][j] = ff_(++counter);
+        }
+    }
+
+    for(auto it : arr){
+        for(auto val : it){
+            std::cout << val.to_float() << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    img vv(arr);
+
+    std::function<ff(img)> func = [](img image) { 
+        auto sum = ff_(0);
+        for(auto i = 0; i < image.rows();++i){
+            for(auto j = 0; j < image.cols();++j){
+                ff::sum(image(i,j),sum,sum);
+            }
+        }
+        return sum;
+    };
+
+    auto res = img::convolution(vv,{3,3},func);
+
+    for(auto i = 0; i < res.rows();++i){
+        for(auto j = 0; j < res.cols();++j){
+            std::cout << res(i,j).to_float() << " ";
+        }
+        std::cout << std::endl;
+    } 
+}
+
 
 #undef ff_
